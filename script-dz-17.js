be9034352c1c7ea2e7bb50a568dcca6f1028c71d
DimaTykva <dimatykvadanait@gmail.com>
2023-02-13 22:00:04 +0200

    dz

---
+++ script-dz-17.js
@@ -0,0 +1,34 @@
+// const declaredVariable;
+const someData = 99;
+const obj = {age: 33};
+//
+// typeof 37;  === number
+// typeof obj; === obj
+// typeof true; === boolean
+// typeof 3.14; === number
+// typeof obj.name; === undefined
+// typeof 42; === number
+// typeof ""; === string
+// typeof "bla"; === string
+// typeof Boolean(1); === true(я думав)  === boolean
+// typeof !!1; === boolean
+// typeof obj.age; === number
+// typeof Infinity;  === не знав === number
+// typeof NaN; === number
+// typeof (someData + " Wisen"); === string
+// typeof Number("1"); === number
+// typeof Number("shoe"); === number - вертає NaN, а NaN це number
+// typeof `template literal`; === string
+// typeof "1"; === string
+// typeof typeof 1; === boolean(я думав) === string
+// typeof false; === boolean
+// typeof String(1);  === string
+// typeof undefined;  === undefined
+// typeof { a: 1 }; === obj
+// typeof declaredVariable; === undefined
+// typeof [1, 2, 4]; === obj
+// typeof null; === obj
+// typeof "John";  === string
+// typeof ([] + '1'); === string
+// typeof (1 + '1'); === string
+// typeof (10 * '2'); === number
